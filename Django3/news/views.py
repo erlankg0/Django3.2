4a957614ac243a10c8163211e659ec1a9bba3409
Erlan <era.ab.02@gmail.com>
2021-04-23 12:39:01 +0300

    news app

--- Django3/news/views.py
+++ Django3/news/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News
 
+
 def index(request):
     news = News.objects.all()
-    res = "<h1>Список новостей</h1>"
-    for item in news:
-        res += f"<div><p>{item.title}</p>" \
-               f"<p>{item.content}</p></div>"
-    return HttpResponse(res)
+    context = {"News": news}
+    return render(request, template_name='news/index.html', context=context)
