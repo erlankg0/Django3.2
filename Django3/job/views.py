8db4c2de9ace6775fb856083811107eac52f6567
Erlan <era.ab.02@gmail.com>
2021-04-24 22:27:58 +0300

    форм

--- Django3/job/views.py
+++ Django3/job/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
-
+from .models import Blog, Author, Entry
+from django.template import loader
 
 def year(request, name, surname, slug):
     date = datetime.date
@@ -13,3 +14,11 @@ def index(request):
     now = datetime.datetime.now()
     html = f"<h1>{now}</h1>"
     return HttpResponse(html)
+
+
+def job(request):
+    blog = Blog.objects.all()
+    author = Author.objects.all()
+    entry = Entry.objects.all()
+    context = {'blog': blog, "author": author, "entry": entry}
+    return render(request, "job/job.html", context)
