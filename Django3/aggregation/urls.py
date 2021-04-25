eeca03c3b13ecd5462d4f55a4b440143af577d1c
Erlan <era.ab.02@gmail.com>
2021-04-25 16:57:06 +0300

    Class view

--- Django3/aggregation/urls.py
+++ Django3/aggregation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookList, MyView
+from .views import BookList, MyView, my_views
 
 urlpatterns = [
     path("", BookList.as_view()),
-    path("mine/", MyView.as_view(), name="my-view")
+    path("mine/", MyView.as_view(), name="my-view"),
+    path("authors/", my_views),
 ]
--- Django3/aggregation/views.py
+++ Django3/aggregation/views.py
@@ -5,10 +5,55 @@ from django.http import HttpResponse
 from django.views import View
 
 
+def my_views(request):
+    if request.method == "GET":
+        author = Author.objects.all()
+        context = {"author": author}
+        return render(request, "aggregation/book_list.html", context=context)
+    else:
+        return HttpResponse("<h1>Error Find 404</h1>")
+
+
 class MyView(View):
+    http_method_names = ['get', 'post']
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("<h1>Hello, World!</h1>")
+        return HttpResponse("""<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Скрипты</title>
+  <style>
+   #msg { 
+    display: none;
+    position: absolute;
+    width: 280px;
+    top: 200px;
+    left: 50%;
+    margin-left: -150px; 
+    background: #fc0;
+    padding: 10px;
+   }
+  </style>
+  <script>
+   function textMsg(msg) {
+    document.getElementById('text').innerHTML = msg;
+    document.getElementById('msg').style.display = 'block';
+   }
+   function closeMsg() {
+    document.getElementById('msg').style.display = 'none';
+   }
+  </script>
+ </head>
+ <body>
+  <div id="msg">
+   <div id="text"></div>
+   <div id="close"><a href="javascript:closeMsg()">[Закрыть]</a></div>
+  </div>
+  <p>Нажмите на <a href="#" onclick="textMsg('Спасибо, что нажали на ссылку!')">  
+    ссылку для открытия сообщения</a>.</p>
+ </body>
+</html>""")
 
 
 class BookList(ListView):
