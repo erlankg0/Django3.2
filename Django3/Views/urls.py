a039115453009a25b3e21f49a479bcc030cc9af1
Erlan <era.ab.02@gmail.com>
2021-04-26 11:42:20 +0300

    Base view

--- Django3/Views/urls.py
+++ Django3/Views/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import PublisherList, PublisherDetail, PublisherDetailOne, BookList
+from .views import AcmeBookList
 
 urlpatterns = [
     path("", PublisherList.as_view()),
     path("detail/<int:pk>", PublisherDetail.as_view()),
     path("details/<int:pk>", PublisherDetailOne.as_view()),
-    path("book/",BookList.as_view()),
+    path("book/", BookList.as_view()),
+    path("acme/<publisher>", AcmeBookList.as_view())
 ]
--- Django3/Views/views.py
+++ Django3/Views/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Publisher, Book
+from django.views.generic import ListView, DetailView, View, TemplateView
+from .models import Publisher, Book, Author
+from django.shortcuts import get_object_or_404
 
 
 class PublisherList(ListView):
@@ -25,7 +26,24 @@ class PublisherDetailOne(DetailView):
 
 
 class BookList(ListView):
-    queryset = Book.objects.order_by("title")
+    queryset = Book.objects.all()
     context_object_name = "book_list"
     template_name = "Views/list_book.html"
+
+
 # Create your views here.
+
+class AcmeBookList(ListView):
+    template_name = "Views/acme_list.html"
+
+    def get_queryset(self):
+        self.publisher = get_object_or_404(PublisherList, name=self.kwargs['publisher'])
+        return Book.objects.filter(publisher=self.publisher)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['publisher'] = self.publisher
+        return context
+
