31b2463502522cecbb7f6d8f123bdd0b092abe1e
Erlan <era.ab.02@gmail.com>
2021-04-26 10:28:51 +0300

    upload to media

--- Django3/Views/urls.py
+++ Django3/Views/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PublisherList
+from .views import PublisherList, PublisherDetail, PublisherDetailOne, BookList
 
 urlpatterns = [
     path("", PublisherList.as_view()),
+    path("detail/<int:pk>", PublisherDetail.as_view()),
+    path("details/<int:pk>", PublisherDetailOne.as_view()),
+    path("book/",BookList.as_view()),
 ]
--- Django3/Views/views.py
+++ Django3/Views/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Publisher
+from django.views.generic import ListView, DetailView
+from .models import Publisher, Book
 
 
 class PublisherList(ListView):
     model = Publisher
     context_object_name = "publishers"
+
+
+class PublisherDetail(DetailView):
+    model = Publisher
+    template_name = 'Views/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['book_list'] = Book.objects.all()
+        return context
+
+
+class PublisherDetailOne(DetailView):
+    context_object_name = "publisherOne"
+    template_name = "Views/detailOne.html"
+    queryset = Publisher.objects.all()
+
+
+class BookList(ListView):
+    queryset = Book.objects.order_by("title")
+    context_object_name = "book_list"
+    template_name = "Views/list_book.html"
 # Create your views here.
